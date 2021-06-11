9c554f2c3c455d4facb7363b3a977a0fdc080e8d
Jonathan Diaz <jonaalbert.adm@gmail.com>
2021-06-11 00:20:59 -0400

    Creacion de proyectos

---
+++ HexToRGB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HexToRGB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
