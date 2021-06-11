ebb50f3608f63019a5487873791ec5f40591b023
Jonathan Diaz <jonaalbert.adm@gmail.com>
2021-06-11 00:44:55 -0400

    Fin

--- HexToRGB/HexToRGBClass.cs
+++ HexToRGB/HexToRGBClass.cs
@@ -8,5 +8,45 @@ namespace HexToRGB
 {
     public class HexToRGBClass
     {
+        public int[] ConvertToRGB(string hex)
+        {
+            if (hex.Length !=6)
+            {
+                throw new ArgumentException("Debe contener 6 digitos");
+            }
+
+            hex = hex.ToUpper();
+            int[] RGB = new int[3];
+            int j = 0;
+
+            for (int i = 0; i < hex.Length; i++)
+            {
+                int hexChar = CharToInt(hex[i]);
+                int hexChar2 = CharToInt(hex[i + 1]);
+                RGB[j] = (hexChar * 16) + hexChar2;
+                i++;
+                j++;
+            }
+            return RGB;
+        }
+
+        public int CharToInt(char c)
+        {
+            try
+            {
+                if (c == 'A') return 10;
+                else if (c == 'B') return 11;
+                else if (c == 'C') return 12;
+                else if (c == 'D') return 13;
+                else if (c == 'E') return 14;
+                else if (c == 'F') return 15;
+                else return int.Parse(c.ToString());
+            }
+            catch (Exception)
+            {
+                throw new ArgumentException("Algun caracter no pertenece al formato hexadecimal");
+            }
+            
+        }
     }
 }
--- HexToRGBTest/HexToRGBClassTest.cs
+++ HexToRGBTest/HexToRGBClassTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using HexToRGB;
 
 namespace HexToRGBTest
 {
@@ -7,12 +8,55 @@ namespace HexToRGBTest
         [SetUp]
         public void Setup()
         {
+            //Hex -> 6 digitos -> base 16
+            //0-9 y 10-15 -> A-F
+            //FF00D4
+            //RGB(r,g,b) -> 0-255
+            //r=FF, g=00, b=D4
+            //FF = 15(16) + 15 = 255
+            //00 = 0(16) + 0 % 16 = 0     }RGB(255,0,212)
+            //D4 = 13(16) + 4 % 16 = 212
         }
 
         [Test]
-        public void Test1()
+        public void ConvertToRGB_Test()
         {
-            Assert.Pass();
+            HexToRGBClass h = new HexToRGBClass();
+            string hex = "FF00D4";
+            int[] expected = { 255, 0, 212 };
+
+            Assert.AreEqual(expected, h.ConvertToRGB(hex));
+        }
+        [Test]
+        public void ConvertToRGB_Test_Uppercase()
+        {
+            HexToRGBClass h = new HexToRGBClass();
+            string hex = "ff00D4";
+            int[] expected = { 255, 0, 212 };
+
+            Assert.AreEqual(expected, h.ConvertToRGB(hex));
+        }
+        [Test]
+        public void ConvertToRGB_Test_Input()
+        {
+            HexToRGBClass h = new HexToRGBClass();
+            string hex = "ff00D454";
+            Assert.That(() => h.ConvertToRGB(hex), Throws.ArgumentException); 
+        }
+
+        [Test]
+        public void CharToInt_Test()
+        {
+            HexToRGBClass h = new HexToRGBClass();
+            char hex = 'F';
+            Assert.AreEqual(15, h.CharToInt(hex));
+        }
+        [Test]
+        public void CharToInt_Test_Character_Exception()
+        {
+            HexToRGBClass h = new HexToRGBClass();
+            char hex = 'M';
+            Assert.That(() => h.CharToInt(hex), Throws.ArgumentException);
         }
     }
 }
